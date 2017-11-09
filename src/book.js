aae292c7052a6a0efdc52378a143016107d87749
raphatapi <raphaeltapioca10@gmail.com>
2017-11-08 20:51:37 -0700

    facebook code for test

--- src/book.js
+++ src/book.js
@@ -47,72 +47,72 @@ var config = {
         }
 
 
-       //  //FACEBOOK AUTHENTICATION
-       //  function statusChangeCallback(response) {
-       //   console.log('statusChangeCallback');
-       //   console.log(response);
-       //   if (response.status === 'connected') {
-       //     testAPI();
-       //   } else {
-       //     document.getElementById('status').innerHTML = 'Please log ' +
-       //       'into this app.';
-       //   }
-       // }
+        //FACEBOOK AUTHENTICATION
+        function statusChangeCallback(response) {
+         console.log('statusChangeCallback');
+         console.log(response);
+         if (response.status === 'connected') {
+           testAPI();
+         } else {
+           document.getElementById('status').innerHTML = 'Please log ' +
+             'into this app.';
+         }
+       }
 
        
-       //    function checkLoginState() {
-       //   FB.getLoginStatus(function(response) {
-       //     statusChangeCallback(response);
-       //   });
-       // }
+          function checkLoginState() {
+         FB.getLoginStatus(function(response) {
+           statusChangeCallback(response);
+         });
+       }
        
 
-       // window.fbAsyncInit = function() {
-       // FB.init({
-       //   appId      : '1994680544133095',
-       //   cookie     : true,  // enable cookies to allow the server to access
-       //                       // the session
-       //   xfbml      : true,  // parse social plugins on this page
-       //   version    : 'v2.8' // use graph api version 2.8
-       // });
+       window.fbAsyncInit = function() {
+       FB.init({
+         appId      : '1994680544133095',
+         cookie     : true,  // enable cookies to allow the server to access
+                             // the session
+         xfbml      : true,  // parse social plugins on this page
+         version    : 'v2.8' // use graph api version 2.8
+       });
 
-       // // Now that we've initialized the JavaScript SDK, we call
-       // // FB.getLoginStatus().  This function gets the state of the
-       // // person visiting this page and can return one of three states to
-       // // the callback you provide.  They can be:
-       // //
-       // // 1. Logged into your app ('connected')
-       // // 2. Logged into Facebook, but not your app ('not_authorized')
-       // // 3. Not logged into Facebook and can't tell if they are logged into
-       // //    your app or not.
-       // //
-       // // These three cases are handled in the callback function.
+       // Now that we've initialized the JavaScript SDK, we call
+       // FB.getLoginStatus().  This function gets the state of the
+       // person visiting this page and can return one of three states to
+       // the callback you provide.  They can be:
+       //
+       // 1. Logged into your app ('connected')
+       // 2. Logged into Facebook, but not your app ('not_authorized')
+       // 3. Not logged into Facebook and can't tell if they are logged into
+       //    your app or not.
+       //
+       // These three cases are handled in the callback function.
 
-       // FB.getLoginStatus(function(response) {
-       //   statusChangeCallback(response);
-       // });
+       FB.getLoginStatus(function(response) {
+         statusChangeCallback(response);
+       });
 
-       // };
+       };
 
-       // // Load the SDK asynchronously
-       // (function(d, s, id) {
-       //   var js, fjs = d.getElementsByTagName(s)[0];
-       //   if (d.getElementById(id)) return;
-       //   js = d.createElement(s); js.id = id;
-       //   js.src = "//connect.facebook.net/en_US/sdk.js";
-       //   fjs.parentNode.insertBefore(js, fjs);
-       // }(document, 'script', 'facebook-jssdk'));
+       // Load the SDK asynchronously
+       (function(d, s, id) {
+         var js, fjs = d.getElementsByTagName(s)[0];
+         if (d.getElementById(id)) return;
+         js = d.createElement(s); js.id = id;
+         js.src = "//connect.facebook.net/en_US/sdk.js";
+         fjs.parentNode.insertBefore(js, fjs);
+       }(document, 'script', 'facebook-jssdk'));
 
-       // // Here we run a very simple test of the Graph API after login is
-       // // successful.  See statusChangeCallback() for when this call is made.
-       // function testAPI() {
-       //   console.log('Welcome!  Fetching your information.... ');
-       //   FB.api('/me', function(response) {
-       //     console.log('Successful login for: ' + response.name);
-       //     document.getElementById('status').innerHTML =
-       //       'Thanks for logging in, ' + response.name + '!';
-       //   });
-       // }
+       // Here we run a very simple test of the Graph API after login is
+       // successful.  See statusChangeCallback() for when this call is made.
+       function testAPI() {
+         console.log('Welcome!  Fetching your information.... ');
+         FB.api('/me', function(response) {
+           console.log('Successful login for: ' + response.name);
+           document.getElementById('status').innerHTML =
+             'Thanks for logging in, ' + response.name + '!';
+         });
+       }
 
 
 $(document).ready(function() {
@@ -191,12 +191,6 @@ $(document).on("click", ".choice", function() {
 
 });
 
-$(document).on("load", ".img-story", function() {
-
-  //identify vars to pull data from the story//
-  
-     });
-
 function storyHTML(currentPage) {
   
 var newGif = stories[currentStory][currentPage].topic;
@@ -219,49 +213,16 @@ var newGif = stories[currentStory][currentPage].topic;
             storyImage.attr("data-state", "still");
 
             storyImage.attr("src", results[0].images.fixed_width.url);
-            // storyImage.attr("data-animate", results[0].images.fixed_width.url); 
-            // storyImage.attr("data-still", results[0].images.fixed_width_still.url);
-            // gifDiv.prepend(storyImage);
 
             storyDiv.append(storyImage);
             console.log(storyDiv);
           });
-       
-      //  $(document).on("click", ".gif", function() {
-      //   // make a variable named state and then store the image's data-state into it. Use the .attr() method for this.
-      //   var state = $(this).attr("data-state");
-
-      //       console.log(state);
-       
-      //   // Check if the variable state is equal to 'still',
-      //   // then update the src attribute of this image to it's data-animate value, and update the data-state attribute to 'animate'.
-
-      //   // If state is equal to 'animate', then update the src attribute of this
-      //   // image to it's data-still value and update the data-state attribute to 'still'
-        
-      //   if(state === "still") {
-      //     var animateURL = $(this).attr("data-animate");
-      //     $(this).attr("src", animateURL);
-      //     $(this).attr("data-state", "animate");
-      //   }
-
-      //   else {
-      //     var stillURL = $(this).attr("data-still");
-      //     $(this).attr("src", stillURL);
-      //       $(this).attr("data-state", "still");
-      //   }
-
-      // });
 
 
 	var storyHTML = "<h1>" + stories[currentStory].storyTitle + "</h1>";
     $(".main-area").html(storyHTML);
     var storyDiv = $("<div>");
     storyDiv.attr("id", "story-div");
-    // var storyImg = $("<img>");
-    // storyImg.addClass("img-story");
-    // storyImg.attr("src", "https://via.placeholder.com/350x350");
-    // storyDiv.append(storyImg);
     var storyText = $("<p>");
     storyText.addClass("text");
     storyText.text(stories[currentStory][currentPage].text);
